74921167e801c1dff28611c10f73efef83ce6bc9
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-11-22 10:15:59 +0100

    switched to es6 import

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const { default: Authentication } = require('../controllers/auth');
-const router = express.Router();
+import {Router} from 'express';
+import  Authentication from '../controllers/auth';
+const router = Router();
 
 
 router.post('/users/login', Authentication.login);
 router.post('/users/signup', Authentication.signup);
 
 
-module.exports = router;
+export default router;
