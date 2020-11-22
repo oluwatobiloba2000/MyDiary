c386723790815bb5ce6b0d03518785ecb5a561bc
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-11-22 10:07:49 +0100

    changed the config names

--- src/db/index.js
+++ src/db/index.js
@@ -5,21 +5,20 @@ dotenv.config();
 let pool;
 if (process.env.NODE_ENV === 'development') {
   pool = new Pool({
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    server: process.env.DATABASE_SERVER,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
   });
 } else {
-  let connectionString = process.env.DB_URL;
+  let connectionString = process.env.DATABASE_URL;
   pool = new Pool({
-    connectionString,
-    rejectUnauthorized: false,
+    connectionString
   });
 }
 pool.on('connect', () => {
-  console.log('DB Connected');
+  console.log('DATABASE Connected');
 });
 
 
